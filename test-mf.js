691b23b2513d33400d877e2823015e292ab6d7a2
Muamaryh <muamaryh26@gmail.com>
2026-08-18 14:06:04 +0700

    cleanup test file

--- test-mf.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { resolveBypass } = require('./services/bypasser');
-
-(async () => {
-  const url = 'https://www.mediafire.com/file/u5p8w9sk9j09vin/GKsTIeO-S2-07-FULLHD-MP4-SAMEHADAKU.CARE.rar/file';
-  const res = await resolveBypass(url);
-  console.log('MediaFire Bypass Result:', JSON.stringify(res, null, 2));
-})();
